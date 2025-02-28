819a5f0d013d503269dacef60bc92a4d7463e62d
seniru-dinil <senirudinil927@gmail.com>
2025-02-28 12:01:20 +0530

    venue class and venue type created

---
+++ src/main/java/edu/icet/dto/Venue.java
@@ -0,0 +1,24 @@
+package edu.icet.dto;
+
+import edu.icet.util.VenueType;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Venue {
+    @NotEmpty(message = "supplier id can not be null")
+    @Positive
+    private Long supplierId;
+    @NotEmpty(message = "location can not be null")
+    private String location;
+    @NotEmpty(message = "venue type can not be null")
+    private VenueType type;
+}
