a97508c5e6cc1372b307a8247eb3094765e5b287
dumidu27730 <mjd2003sachinthaka@gmail.com>
2025-02-28 13:18:20 +0530

    complete agenda dto

--- src/main/java/edu/icet/dto/Agenda.java
+++ src/main/java/edu/icet/dto/Agenda.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -13,7 +12,7 @@ import java.time.LocalTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+
 public class Agenda {
     @NotBlank
     private Integer id;
