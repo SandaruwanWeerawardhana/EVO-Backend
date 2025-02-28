363e5ab25bac9955ddac517061075084796d2532
SandhanuDulmeth <sandhanu12@gmail.com>
2025-02-28 13:12:50 +0530

    Add User DTO class

---
+++ src/main/java/edu/icet/dto/User.java
@@ -0,0 +1,30 @@
+package edu.icet.dto;
+
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+
+    @NotEmpty(message = "User ID should not be blank")
+    private Integer userId;
+    @NotEmpty(message = "User name should not be blank")
+    private String userName;
+    private String password;
+    @Email(message = "Email should be valid")
+    private String email;
+    @PastOrPresent(message = "Registered date should be in past or present")
+    private String registeredDate;
+
+
+}
