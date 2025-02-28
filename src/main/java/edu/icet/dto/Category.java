ef0abe6e95f29f3697d529b8425ee7dba38fa0cb
Ruchira Ranasinghe <rhvruchira@gmail.com>
2025-02-28 13:14:10 +0530

    Category class created

--- src/main/java/edu/icet/dto/Category.java
+++ src/main/java/edu/icet/dto/Category.java
@@ -6,17 +6,14 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 public class Category {
     @NotNull(message = "ID cannot be null")
     @Min(value = 1,message = "ID must be greater than or equal to 1")
     private long id;
-
     @NotBlank(message = "Name cannot be blank")
     private String name;
 }
