97289fcf9213853e8c761146b5e8da95e33eaeaf
Ruvindu <ruvindusharada22@gmail.com>
2025-02-28 14:01:07 +0530

    create Review dto

--- src/main/java/edu/icet/dto/Review.java
+++ src/main/java/edu/icet/dto/Review.java
@@ -1,7 +1,6 @@
 package edu.icet.dto;
 
 import edu.icet.util.Rating;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.*;
@@ -13,12 +12,14 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Review {
-    @NotBlank(message = "Review id cannot be Blank")
+
     private long reviewId;
     @NotNull(message = "Rating cannot be Null")
     private Rating rating;
     @PastOrPresent
+    @NotNull(message = "Date cannot be Null")
     private LocalDate date;
+    @NotNull(message = "comment can not be null")
     private String comment;
 
 }
