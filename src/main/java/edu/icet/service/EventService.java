6740ebf8506822379e758b187d707356d5a89cc3
sandeepa <sanvidurath98@gmail.com>
2025-03-04 09:59:06 +0530

    event service

---
+++ src/main/java/edu/icet/service/EventService.java
@@ -0,0 +1,20 @@
+package edu.icet.service;
+
+import edu.icet.dto.Event;
+import edu.icet.dto.Location;
+import edu.icet.util.EventType;
+
+import java.util.List;
+
+public interface EventService {
+    boolean createEvent(Event event);
+    boolean updateEvent(Event event, Integer id);
+    boolean updateEvent(Integer id);
+    Event searchEvent(Integer id);
+    boolean deleteEvent(Integer id);
+
+    List<Event> getAll();
+
+    List<Event> getEventsByLocation(Location location);
+    List<Event> getEventsByEventType(EventType eventType);
+}
