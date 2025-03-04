392e9f55f25853c2aca089a5425941d232d638fa
methnii <methniranasingha.gmail.com>
2025-03-04 12:11:36 +0530

    OutdoorAreaService,PoolService,PropertyImageService

---
+++ src/main/java/edu/icet/service/PoolService.java
@@ -0,0 +1,18 @@
+package edu.icet.service;
+
+import edu.icet.dto.Pool;
+
+import java.util.List;
+
+public interface PoolService {
+    List <Pool> getAll();
+    Pool save (Pool pool);
+    Boolean delete (Long id);
+    Boolean update (Pool pool);
+
+
+
+
+
+
+}
