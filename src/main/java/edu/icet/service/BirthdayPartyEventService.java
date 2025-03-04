1b7e4966540686e30a434c3a352a914323c4e4b7
Madhubhashini57 <jkpimesha2003@gmail.com>
2025-03-04 11:13:27 -0800

    create BirthdayDto and implement BirthdayPartyEventService

---
+++ src/main/java/edu/icet/service/BirthdayPartyEventService.java
@@ -0,0 +1,19 @@
+package edu.icet.service;
+
+import edu.icet.dto.BirthdayParty;
+
+import java.util.Date;
+import java.util.List;
+
+public interface BirthdayPartyEventService {
+    BirthdayParty getBirthday (Integer id);
+    BirthdayParty getBirthday (String ownerName);
+    List<BirthdayParty> getAll ();
+    List<BirthdayParty> getAll (Date date);
+    //One user can create multiple birthday parties.Though their username get all
+    List<BirthdayParty> getAll (String username);
+    Boolean save (BirthdayParty birthdayParty);
+    Boolean delete (BirthdayParty birthdayParty);
+    Boolean delete (Integer id);
+    Boolean update (BirthdayParty birthdayParty);
+}
