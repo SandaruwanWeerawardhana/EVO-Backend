e93dd39fcb976e5a9b8f11d3b7b7c1e5d87cd524
Sahan Chamara <167575715+SahanChamara@users.noreply.github.com>
2025-03-04 13:28:05 +0530

    Update AdminService.java

--- src/main/java/edu/icet/service/AdminService.java
+++ src/main/java/edu/icet/service/AdminService.java
@@ -6,9 +6,9 @@ import java.util.List;
 public interface AdminService {
      boolean addAdmin (Admin admin);
      boolean deleteAdmin (Integer adminID);
-     boolean updateAdmin(Admin admin);
-     Admin getAdminById(Integer adminId);
-     boolean adminExists(Integer adminId);
+     boolean updateAdmin(Integer adminID,Admin admin);
+     Admin getAdminById(Integer adminID);
+     boolean adminExists(Integer adminID);
      int countAdmins();
      boolean changeAdminType(AdminType type);
      List<Admin>getAllAdmins();
