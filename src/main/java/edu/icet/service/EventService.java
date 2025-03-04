8c9e1cec0dacfb3d2bea7a65402de456afbc97dd
sandeepa <sanvidurath98@gmail.com>
2025-03-04 10:29:37 +0530

    removed whitespace and create method

--- src/main/java/edu/icet/service/EventService.java
+++ src/main/java/edu/icet/service/EventService.java
@@ -7,14 +7,11 @@ import edu.icet.util.EventType;
 import java.util.List;
 
 public interface EventService {
-    boolean createEvent(Event event);
     boolean updateEvent(Event event, Integer id);
     boolean updateEvent(Integer id);
     Event searchEvent(Integer id);
     boolean deleteEvent(Integer id);
-
     List<Event> getAll();
-
     List<Event> getEventsByLocation(Location location);
     List<Event> getEventsByEventType(EventType eventType);
 }
