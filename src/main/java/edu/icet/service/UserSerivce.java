954c242c79e5a808dde868aea17315cced6c6fd8
SandhanuDulmeth <sandhanu12@gmail.com>
2025-03-04 10:23:43 +0530

    Done AuditHistorySerivce and UserSerivce

---
+++ src/main/java/edu/icet/service/UserSerivce.java
@@ -0,0 +1,23 @@
+package edu.icet.service;
+
+import edu.icet.dto.User;
+
+import java.util.List;
+
+
+public interface UserSerivce {
+     Boolean saveUser(User user);
+     User getUser(String username);
+     Boolean updateUser(User user);
+     Boolean deleteUser(String username);
+     List<User> getAllUsers();
+     boolean isUserExist(String username);
+     boolean isUserExist(User user);
+
+
+
+
+
+
+
+}
