7dc0c33fb2e74a274832a341e86ecafc21b244e2
Madhubhashini57 <jkpimesha2003@gmail.com>
2025-03-04 13:48:12 -0800

    Update CrossOrigin

--- src/main/java/edu/icet/controller/AnniversaryEventController.java
+++ src/main/java/edu/icet/controller/AnniversaryEventController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/anniversary")
 @RequiredArgsConstructor
+@CrossOrigin
 public class AnniversaryEventController {
 
    private final AnniversaryEventService anniversaryEventService;
