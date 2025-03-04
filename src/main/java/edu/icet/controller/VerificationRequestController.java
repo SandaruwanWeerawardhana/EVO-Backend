799d0844bc232a1548c0887e27c27f07306f0e4d
NisanduGimhan <nisanduab@gmail.com>
2025-03-04 15:55:07 +0530

    Create AuditHistoryController

--- src/main/java/edu/icet/controller/VerificationRequestController.java
+++ src/main/java/edu/icet/controller/VerificationRequestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/verificationRequest")
+@RequestMapping("/supplierverificationRequest")
 @RequiredArgsConstructor
 @CrossOrigin
 public class VerificationRequestController {
