004eaecea996f187897ba2abff31ddfd96d59c24
Sithum9999 <sankalpasithum51@gmail.com>
2025-03-04 13:33:26 +0530

    Fix MusicService Interface

--- src/main/java/edu/icet/service/MusicService.java
+++ src/main/java/edu/icet/service/MusicService.java
@@ -10,7 +10,7 @@ public interface MusicService {
 
     boolean addMusic(Music music);
 
-    Music searchMusic(Music music);
+    Music searchMusic(String qurey);
 
     boolean updateMusic(Music music);
 
