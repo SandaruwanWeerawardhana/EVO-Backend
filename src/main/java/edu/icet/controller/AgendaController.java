47ff7e29c3615d4bad829e4d38e44096d7fccbc3
tharushi330 <tharushiwarangana78@gmail.com>
2025-03-04 13:50:48 +0530

    Create AgendaController Tharushi

---
+++ src/main/java/edu/icet/controller/AgendaController.java
@@ -0,0 +1,41 @@
+package edu.icet.controller;
+
+import edu.icet.dto.Agenda;
+import edu.icet.service.AgendaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/agenda")
+@RequiredArgsConstructor
+@CrossOrigin
+public class AgendaController {
+    AgendaService agendaService;
+
+    @PostMapping("/create")
+    public boolean create(@RequestBody Agenda agenda){
+        return agendaService.create(agenda);
+    }
+
+    @GetMapping("/get-all")
+    public List<Agenda> getAll(){
+        return agendaService.getAll();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable Integer id){
+        return agendaService.delete(id);
+    }
+
+    @PutMapping("/update")
+    public boolean update(@RequestBody Agenda agenda){
+        return agendaService.update(agenda);
+    }
+
+    @GetMapping("/get/{id}")
+    public Agenda getById(@PathVariable Integer id){
+        return agendaService.getById(id);
+    }
+}
