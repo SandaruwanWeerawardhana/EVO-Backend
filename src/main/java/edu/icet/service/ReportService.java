deeef2482eec5b2701151a787636b315c3549c4e
Akash Fernando <Sathsarafernando11@gmail.com>
2025-03-04 11:50:09 +0530

    Search History service and Report service

--- src/main/java/edu/icet/service/ReportService.java
+++ src/main/java/edu/icet/service/ReportService.java
@@ -1,14 +1,15 @@
 package edu.icet.service;
 
 import edu.icet.dto.Report;
+import edu.icet.util.ReportType;
+
 import java.util.List;
 
 public interface ReportService {
     boolean saveReport(Report report);
     boolean updateReport(Report report);
-    boolean deleteReport(Report report);
+    boolean deleteReportById(Long reportId);
     Report searchReport(long reportId);
-    Report getReportById(long reportId);
     List<Report> getAllReports();
-    List<Report> getFillterReports();
+    List<Report> getFillterReports(ReportType reportType);
 }
--- src/main/java/edu/icet/service/SearchHistoryService.java
+++ src/main/java/edu/icet/service/SearchHistoryService.java
@@ -6,11 +6,9 @@ import java.util.List;
 
 public interface SearchHistoryService {
     boolean saveSearchHistory(SearchHistory searchHistory);
-    boolean deleteSearchHistory(SearchHistory report);
     boolean deleteSearchHistoryById(long searchId);
     SearchHistory searchSearchHistory(long searchId);
-    SearchHistory getSearchHistoryById(long searchId);
     List<SearchHistory> getAllSearchHistory();
-    List<SearchHistory> getFillterSearchHistory();
-    List<SearchHistory>getTopSearchHistory();
+    List<SearchHistory> getFillterSearchHistory(String title);
+
 }
