26d04be818a1d670b251e33be45df94cd040b833
minindu-alwis <minindualwis@gmail.com>
2025-03-04 13:16:42 +0530

    booking slot

---
+++ src/main/java/edu/icet/service/BookingSlotService.java
@@ -0,0 +1,16 @@
+package edu.icet.service;
+
+import edu.icet.dto.BookingSlot;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface BookingSlotService {
+
+    List<BookingSlot> getAll();
+    BookingSlot save(BookingSlot bookingSlot);
+    Boolean delete(BookingSlot bookingSlot);
+    Boolean delete(Long id);
+    BookingSlot update(BookingSlot bookingSlot);
+    List<BookingSlot> search(Long propertyId, LocalDateTime startTime, LocalDateTime endTime);
+}
