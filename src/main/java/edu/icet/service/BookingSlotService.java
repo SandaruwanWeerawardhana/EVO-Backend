6b3f5594b3f07c79c0cb7990de7f96ffde72d3da
minindu-alwis <minindualwis@gmail.com>
2025-03-04 16:35:57 +0530

    BeautyPackageController , PhotographerImageController, PhotographerPackageController,SalonImageController remove bookingslot delete with object

--- src/main/java/edu/icet/service/BookingSlotService.java
+++ src/main/java/edu/icet/service/BookingSlotService.java
@@ -9,7 +9,6 @@ public interface BookingSlotService {
 
     List<BookingSlot> getAll();
     BookingSlot save(BookingSlot bookingSlot);
-    Boolean delete(BookingSlot bookingSlot);
     Boolean delete(Long id);
     BookingSlot update(BookingSlot bookingSlot);
     List<BookingSlot> search(Long propertyId, LocalDateTime startTime, LocalDateTime endTime);
