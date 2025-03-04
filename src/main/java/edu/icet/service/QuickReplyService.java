af81469ca53ab59f491085447dbe8bfbeb8252f7
SandaruwanWeerawardhana <weerawadanasw@gmail.com>
2025-03-04 13:26:09 +0530

    create Quick Reply service2

--- src/main/java/edu/icet/service/QuickReplyService.java
+++ src/main/java/edu/icet/service/QuickReplyService.java
@@ -1,12 +1,15 @@
 package edu.icet.service;
 
+import edu.icet.dto.QuickReplies;
+import edu.icet.dto.Supplier;
+
 import java.util.List;
 
-public interface QuickReplies {
+public interface QuickReplyService {
 
     Boolean save(QuickReplies quickReplies);
     Boolean delete(Long id);
     Boolean update (QuickReplies quickReplies);
-    List<QuickReplies> getAll();
+    List<QuickReplies> getAll(Supplier supplier);
 
 }
