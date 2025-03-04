a93877317a22b36426bee17c61c97c2f8bb43928
PBinush <pasidbinush@gmail.com>
2025-03-04 10:21:05 +0530

    event report service interface

---
+++ src/main/java/edu/icet/service/EventReportService.java
@@ -0,0 +1,16 @@
+package edu.icet.service;
+
+import edu.icet.dto.EventReport;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface EventReportService {
+    boolean saveEventReport(EventReport eventReport);
+    boolean updateEventReport(Long id,EventReport eventReport);
+    EventReport getEventReportById(Long id);
+    List<EventReport> getAllEventReports();
+    List<EventReport> getAllEventReportsByLocation(String location);
+    List<EventReport> getAllEventReportsByDate(LocalDate date);
+    List<EventReport> getAllEventReportsByEventType(String eventType);
+    boolean deleteEventReportById(Long id);
+}
