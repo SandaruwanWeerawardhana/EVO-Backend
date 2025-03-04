47d123730d74f3f846e2572f49bad61b22eca846
Madhubhashini57 <jkpimesha2003@gmail.com>
2025-03-04 13:44:27 -0800

    Update AnniversaryEventService and implement AnniversaryEventController

---
+++ src/main/java/edu/icet/controller/AnniversaryEventController.java
@@ -0,0 +1,47 @@
+package edu.icet.controller;
+
+import edu.icet.dto.Anniversary;
+import edu.icet.service.AnniversaryEventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/anniversary")
+@RequiredArgsConstructor
+public class AnniversaryEventController {
+
+   private final AnniversaryEventService anniversaryEventService;
+
+    @PostMapping("/save")
+    public boolean addAnniversary(@RequestBody Anniversary anniversary) {
+        return anniversaryEventService.add(anniversary);
+    }
+
+    @GetMapping("/get-all")
+    public List<Anniversary> getAllAnniversaries() {
+        return anniversaryEventService.getAll();
+    }
+
+    @GetMapping("/get/{eventId}")
+    public Anniversary getAnniversary(@PathVariable("eventId") Integer eventId) {
+        return anniversaryEventService.get(eventId);
+    }
+
+    @PutMapping("/update")
+    public boolean updateAnniversary(@RequestBody Anniversary anniversary) {
+        return anniversaryEventService.update(anniversary);
+    }
+
+    @DeleteMapping("/delete/{eventId}")
+    public boolean deleteAnniversary(@PathVariable Integer eventId) {
+       return anniversaryEventService.delete(eventId);
+    }
+}
+
+
+
+
+
+
