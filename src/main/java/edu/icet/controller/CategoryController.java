6bc0650b4a6e967e66dfe77fee855f3b4375eb0f
SandaruwanWeerawardhana <weerawadanasw@gmail.com>
2025-03-04 16:01:30 +0530

    create CategoryController class

---
+++ src/main/java/edu/icet/controller/CategoryController.java
@@ -0,0 +1,46 @@
+package edu.icet.controller;
+
+import edu.icet.dto.Category;
+import edu.icet.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("supplier/category")
+@CrossOrigin
+public class CategoryController {
+    final CategoryService service;
+
+    @PostMapping("/add")
+    public Category addCategory(@RequestBody Category category){
+        return service.save(category);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Boolean deleteCategoryById(@PathVariable Long id){
+        return service.delete(id);
+    }
+
+    @DeleteMapping("/delete")
+    public Boolean deleteCategory(@RequestBody Category category){
+        return service.delete(category);
+    }
+
+    @PutMapping("/update")
+    public Category updateCategory(@RequestBody Category category){
+        return service.update(category);
+    }
+
+    @GetMapping("/search")
+    public Category searchCategory(@PathVariable String query){
+        return service.search(query);
+    }
+
+    @GetMapping("/get-all")
+    public List<Category> getAllCategory(){
+        return service.getAll();
+    }
+}
