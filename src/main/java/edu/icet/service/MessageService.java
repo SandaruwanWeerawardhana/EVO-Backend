c35f1607da9679778e07fded8624564038f347ed
Bhashini123868 <bhashimunasinghe286@gmail.com>
2025-03-04 10:30:08 +0530

    MessageService

---
+++ src/main/java/edu/icet/service/MessageService.java
@@ -0,0 +1,13 @@
+package edu.icet.service;
+import edu.icet.dto.Message;
+import java.util.List;
+
+public interface MessageService {
+    void sendMessage(Message message);
+    void deleteMessage(Long mid );
+    List<Message> getAllMessages();
+    Message getMessageById(Long mid);
+    boolean isMessageDelivered(Long mid);
+    boolean isMessageRead(Long mid);
+    List<Message> searchMessage(Long mid);
+}
