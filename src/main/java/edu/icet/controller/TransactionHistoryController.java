cf8e1fb88ae856d4658d9e4a2f9619817dd7ed0d
RuwanDilhara <roshansameera916@gmail.com>
2025-03-04 15:27:17 +0530

    created TransactionHistoryController class

--- src/main/java/edu/icet/controller/TransactionHistoryController.java
+++ src/main/java/edu/icet/controller/TransactionHistoryController.java
@@ -1,14 +1,50 @@
 package edu.icet.controller;
 
+import edu.icet.dto.TransactionHistory;
+import edu.icet.service.TransactionHistoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping
+@RequestMapping("api/v1/transactions/history")
 @RequiredArgsConstructor
 public class TransactionHistoryController {
 
+    final TransactionHistoryService service;
+
+    @PostMapping("/save")
+    public ResponseEntity<Boolean> saveTransactionHistory(@RequestBody TransactionHistory history){
+        TransactionHistory transactionHistory = service.save(history);
+        return ResponseEntity.ok(transactionHistory==null);
+    }
+    @GetMapping("/getAll")
+    public ResponseEntity<List<TransactionHistory>> getAllTransactionHistory(){
+        List<TransactionHistory> all = service.getAll();
+        return ResponseEntity.ok(all);
+    }
+    @GetMapping("/byId/{id}")
+    public ResponseEntity<TransactionHistory>getTransactionHistoryById(@PathVariable Long id){
+        TransactionHistory transactionHistoryById = service.getTransactionHistoryById(id);
+        return ResponseEntity.ok(transactionHistoryById);
+    }
+    @GetMapping("/byDate/{date}")
+    public ResponseEntity<List<TransactionHistory>> getTransactionByDate(@PathVariable LocalDate date){
+        List<TransactionHistory> transactionByDate = service.getTransactionByDate(date);
+        return ResponseEntity.ok(transactionByDate);
+    }
+    @GetMapping("/byUserId/{id}")
+    public ResponseEntity<List<TransactionHistory>> getTransactionByUserId(@PathVariable Long id){
+        List<TransactionHistory> transactionByUserId = service.getTransactionByUserId(id);
+        return ResponseEntity.ok(transactionByUserId);
+    }
+    @DeleteMapping("/deleteById/{id}")
+    public ResponseEntity<Boolean> deleteTransactionHistoryById(@PathVariable Long id){
+        boolean b = service.deleteTransactionHistoryById(id);
+        return ResponseEntity.ok(b);
+    }
 }
