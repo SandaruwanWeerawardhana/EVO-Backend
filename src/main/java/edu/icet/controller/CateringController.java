5f6508320ce4c50e54941407f89d4b8f22dd04f0
Kalum Gunawardhana <kguna415@gmail.com>
2025-03-04 15:47:44 +0530

    create crontroller class

--- src/main/java/edu/icet/controller/CateringController.java
+++ src/main/java/edu/icet/controller/CateringController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/catering")
+@RequestMapping("/supplier/catering")
 @RequiredArgsConstructor
 @CrossOrigin
 public class CateringController {
