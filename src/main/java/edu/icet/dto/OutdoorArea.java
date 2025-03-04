3f61147de9daa0283de2bd05db62dd621ad0597b
methnii <methniranasingha.gmail.com>
2025-03-04 09:49:26 +0530

    OutdoorArea dto class

--- src/main/java/edu/icet/dto/OutdoorArea.java
+++ src/main/java/edu/icet/dto/OutdoorArea.java
@@ -1,5 +1,6 @@
 package edu.icet.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +12,15 @@ import lombok.Setter;
 @Setter
 
 
-
 public class OutdoorArea {
-
-
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private double size;
+    private Boolean seatingCapacity;
+    private Boolean lighting;
+    private Boolean weatherProtection;
 
 
 }
