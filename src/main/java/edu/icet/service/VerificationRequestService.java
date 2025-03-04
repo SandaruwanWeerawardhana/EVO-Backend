e03fc08482bfc4b2dc9066fe4980c4be9c5ff982
NisanduGimhan <nisanduab@gmail.com>
2025-03-04 11:25:05 +0530

    Change AuditLogdto to AuditLog,Change VerificationRequestD to VerificationRequest and also create verificationRequestService interface

--- src/main/java/edu/icet/service/VerificationRequestService.java
+++ src/main/java/edu/icet/service/VerificationRequestService.java
@@ -1,13 +1,15 @@
 package edu.icet.service;
 
+import edu.icet.dto.VerificationRequest;
+
 import java.util.List;
 
 public interface VerificationRequestService {
 
- VerificationRequestService saveVerificationRequest(VerificationRequestService request);
- VerificationRequestService findVerificationrequestbyId(Long id);
- List<VerificationRequestService> getAllVerificationRequest();
+ boolean saveVerificationRequest(VerificationRequest request);
+ VerificationRequest findVerificationrequestbyId(Long id);
+ List<VerificationRequest> getAllVerificationRequest();
  boolean deleteVerificationRequest(Long id);
- VerificationRequestService updateVerificationRequest(VerificationRequestService request);
+ boolean updateVerificationRequest(VerificationRequest request);
 
 }
