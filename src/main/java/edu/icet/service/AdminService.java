7ee35b327f7861a067d407a4991e6734c2dd47c8
amashikaushala <amashikaushala2003@gmail.com>
2025-03-05 09:09:00 +0530

    AdminController

--- src/main/java/edu/icet/service/AdminService.java
+++ src/main/java/edu/icet/service/AdminService.java
@@ -1,16 +1,14 @@
 package edu.icet.service;
 import edu.icet.dto.Admin;
-import edu.icet.util.AdminType;
 
 import java.util.List;
 public interface AdminService {
      boolean addAdmin (Admin admin);
      boolean deleteAdmin (Integer adminID);
      boolean updateAdmin(Integer adminID,Admin admin);
-     Admin getAdminById(Integer adminID);
+     boolean getAdminById(Integer adminID);
      boolean adminExists(Integer adminID);
-     int countAdmins();
-     boolean changeAdminType(AdminType type);
+     boolean countAdmins();
      List<Admin>getAllAdmins();
-     List<Admin>getAdminByType(String type);
+     List<Admin>getAdminByType();
 }
