96a8bcde631e8e80b4fc18f52ae8f6fdf12ea80e
Sahan Chamara <sahanchamara456@gmail.com>
2025-03-11 11:44:29 +0530

    supplier chatImage service impl  and Chat Service impl

--- src/main/java/edu/icet/service/system/impl/ChatServiceImpl.java
+++ src/main/java/edu/icet/service/system/impl/ChatServiceImpl.java
@@ -4,20 +4,30 @@ import edu.icet.dto.Chat;
 import edu.icet.service.system.ChatService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ChatServiceImpl implements ChatService {
+    private List<Chat> chatList = new ArrayList<>();
+
     @Override
-    public boolean create(Chat chatService) {
-        return false;
+    public boolean create(Chat chat) {
+        return chatList.add(chat);
     }
 
     @Override
     public Chat getChat(Integer id) {
+        for (Chat chat:chatList){
+            if (chat.getId().equals(id)){
+                return chat;
+            }
+        }
         return null;
     }
 
     @Override
     public boolean delete(Integer id) {
-        return false;
+        return chatList.removeIf(chat -> chat.getId().equals(id));
     }
 }
