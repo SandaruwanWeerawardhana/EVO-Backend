cf8fc915ad9c2bb126fc4109be5105c4f3fbef6f
Thenujana <chenarathenujana@gmail.com>
2025-03-11 11:14:00 +0530

    Reply service and ReplyServiceImpl

--- src/main/java/edu/icet/service/system/ReplyService.java
+++ src/main/java/edu/icet/service/system/ReplyService.java
@@ -7,11 +7,11 @@ import java.util.List;
 
 public interface ReplyService {
 
-    void addReply(Reply reply);
+    List<Reply> addReply(Reply reply);
 
     List<Reply>getAllReplies();
 
-    void updateReply(Reply reply);
+    Reply updateReply(Reply reply);
 
     void deleteReplyById(Long replyId);
 
--- src/main/java/edu/icet/service/system/impl/ReplyServiceImpl.java
+++ src/main/java/edu/icet/service/system/impl/ReplyServiceImpl.java
@@ -1,44 +1,66 @@
 package edu.icet.service.system.impl;
 
 import edu.icet.dto.Reply;
-import edu.icet.service.system.ReplyService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 @Service
-public class ReplyServiceImpl implements ReplyService {
-    @Override
-    public void addReply(Reply reply) {
+@RequiredArgsConstructor
 
+public class ReplyServiceImpl implements edu.icet.service.system.ReplyService {
+    private final ModelMapper mapper;
+    private List<Reply>replyList=new ArrayList<>();
+    @Override
+    public List<Reply> addReply(Reply reply) {
+        replyList.add(reply);
+        return replyList;
     }
 
     @Override
     public List<Reply> getAllReplies() {
-        return List.of();
+        return replyList;
     }
 
     @Override
-    public void updateReply(Reply reply) {
-
+    public Reply updateReply(Reply reply) {
+        for (int i=0;i<replyList.size();i++) {
+            if(replyList.get(i).getReplyId().equals(reply.getReplyId())){
+                replyList.set(i, reply);
+                return reply;
+            }
+        }
+        return null;
     }
+
 
     @Override
     public void deleteReplyById(Long replyId) {
-
+        replyList.removeIf(reply -> reply.getReplyId().equals(replyId));
     }
 
     @Override
     public Reply searchReplyById(Long replyId) {
-        return null;
+        return replyList.stream()
+                .filter(reply -> reply.getReplyId().equals(replyId))
+                .findFirst()
+                .orElse(null);
     }
+
 
     @Override
     public Reply searchReplyByUserId(Long userId) {
-        return null;
+        return replyList.stream()
+                .filter(reply -> reply.getUserId().equals(userId))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public Boolean containsBadWords(String reply) {
+
         return null;
     }
 }
