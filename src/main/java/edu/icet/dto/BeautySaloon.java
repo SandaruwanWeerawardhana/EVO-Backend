709e060fa988ec65c56fa6d8d7e0e2b9d15e082c
yetimeh <senukabandara2006@gmail.com>
2025-03-11 18:02:23 +0530

    hide unused controllers from documentation fix main supplier  DTOs

--- src/main/java/edu/icet/dto/BeautySaloon.java
+++ src/main/java/edu/icet/dto/BeautySaloon.java
@@ -14,31 +14,9 @@ import lombok.Setter;
 @Getter
 
 public class BeautySaloon {
-    @NotNull
     private Long id;
 
-    @NotBlank
-    private String name;
+    @NotNull
+    private Long supplierID;
 
-    @NotBlank
-    private String telNumber;
-
-    @NotBlank
-    private String email;
-
-    public void setTelNumber(String telNumber) {
-        if (telNumber != null && telNumber.length() == 10 && telNumber.chars().allMatch(Character::isDigit)) {
-            this.telNumber = telNumber;
-        } else {
-            throw new IllegalArgumentException("Telephone number must be 10 digits");
-        }
-    }
-
-    public void setEmail(String email) {
-        if (email != null && email.endsWith(".com") && email.contains("@") && email.indexOf("@") > 0 && email.indexOf("@") < email.indexOf(".")) {
-            this.email = email;
-        } else {
-            throw new IllegalArgumentException("Email number must be a valid .com email address");
-        }
-    }
 }
