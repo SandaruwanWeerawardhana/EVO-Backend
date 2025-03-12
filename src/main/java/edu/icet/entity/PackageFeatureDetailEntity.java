a56123f00abc0e54a8a4e73ce222116139993504
kaveeshaSanjana <kapilakarunarathna@gmail.com>
2025-03-12 17:02:08 +0530

    Implement OutdoorAreaEntity.java and PackageFeatureDetailEntity.java

---
+++ src/main/java/edu/icet/entity/PackageFeatureDetailEntity.java
@@ -0,0 +1,21 @@
+package edu.icet.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Table(name = "package_feature_detail")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PackageFeatureDetailEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long packageFeatureDetailId;
+    @Column(name = "package_id",nullable = false)
+    private Long packageId;
+    @Column(name = "feature_id",nullable = false)
+    private Long featureId;
+}
