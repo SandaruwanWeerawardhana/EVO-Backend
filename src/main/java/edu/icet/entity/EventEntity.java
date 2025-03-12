a035bb310d2d01e9d865ede5153d121abf47bde3
kaveeshaSanjana <kapilakarunarathna@gmail.com>
2025-03-12 15:12:17 +0530

    Implement EventEntity.java

---
+++ src/main/java/edu/icet/entity/EventEntity.java
@@ -0,0 +1,52 @@
+package edu.icet.entity;
+
+import edu.icet.dto.Location;
+import edu.icet.util.BudgetType;
+import edu.icet.util.EventStatusType;
+import edu.icet.util.EventType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+import java.util.Date;
+@Entity
+@Table(name = "event")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer eventId;
+
+    @NotNull
+    private Long userId;
+
+    @NotNull @Future
+    private Date eventDate;
+
+    @NotNull
+    private Time startTime;
+
+    @NotNull
+    private Time endTime;
+
+    private Location location;
+
+    @NotNull
+    private EventType eventType;
+
+    @NotNull @Min(1)
+    private Integer capacity;
+
+    @NotNull
+    private BudgetType budgetType;
+
+    @NotNull
+    private EventStatusType eventStatus;
+}
