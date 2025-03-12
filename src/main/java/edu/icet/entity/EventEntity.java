ed4cb38c9de46059de6b823d57f2c392fd372913
kaveeshaSanjana <kapilakarunarathna@gmail.com>
2025-03-12 16:16:28 +0530

    Implement EventEntity.java

--- src/main/java/edu/icet/entity/EventEntity.java
+++ src/main/java/edu/icet/entity/EventEntity.java
@@ -22,6 +22,7 @@ import java.util.Date;
 public class EventEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer eventId;
 
     @NotNull
