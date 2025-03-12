9f03dd17d40bb7f75bd8d0fefb5c8f48bbfb6d5d
Sash980402 <kesharasasanka120@gmail.com>
2025-03-12 15:20:30 +0530

    create AgendaEntity_event

---
+++ src/main/java/edu/icet/entity/AgendaEntity.java
@@ -0,0 +1,33 @@
+package edu.icet.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "agenda")
+public class AgendaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "time", nullable = false)
+    private LocalTime time;
+
+    @Column(name = "agendadetails", nullable = false)
+    private String agendaDetail;
+}
