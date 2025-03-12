389f1633309a09f13524fe1665bcb3071761854f
Sash980402 <kesharasasanka120@gmail.com>
2025-03-12 15:49:58 +0530

    create WeddingEntity_event

---
+++ src/main/java/edu/icet/entity/WeddingEntity.java
@@ -0,0 +1,30 @@
+package edu.icet.entity;
+
+import edu.icet.util.WeddingType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "wedding")
+public class WeddingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "weddingid", nullable = false)
+    private String weddingId;
+
+    @Column(name = "weddingtype", nullable = false)
+    private WeddingType weddingType;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+}
