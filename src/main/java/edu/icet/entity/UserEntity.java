45d4a83b4be69325d0f1380ba731a3a3f2552204
NisanduGimhan <nisanduab@gmail.com>
2025-03-18 09:00:59 +0530

    create user ,verificationrequest and admin entities

---
+++ src/main/java/edu/icet/entity/UserEntity.java
@@ -0,0 +1,44 @@
+package edu.icet.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.usertype.UserType;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
+@Table(name = "User")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull(message = "User ID should not be blank")
+    private Long userId;
+    @NotEmpty(message = "First Name should not be blank")
+    private String firstName;
+    @NotEmpty(message = "Last Name should not be blank")
+    private String lastName;
+    @NotNull
+    private String password;
+    @Email(message = "Email should be valid")
+    private String email;
+    @PastOrPresent(message = "Registered date should be in past or present")
+    private String registeredDate;
+    @NotEmpty(message = "User type should not be blank")
+    private UserType userType;
+    @NotEmpty(message = "Contact Number should not be blank")
+    private String contactNumber;
+    @NotEmpty(message = "Address should not be blank")
+    private String address;
+    @NotEmpty(message = "City should not be blank")
+    private String city;
+}
