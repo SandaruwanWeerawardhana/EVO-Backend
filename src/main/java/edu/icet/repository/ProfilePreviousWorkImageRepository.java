5183e964737bfb0f023a3f0acacaaabc406886c2
minindu-alwis <minindualwis@gmail.com>
2025-03-18 09:35:16 +0530

    ProfilePackagesEntity ProfilePackageRepository ProfilePreviousWorkImageRepository ProfilePreviousWorkImageEntity Done

--- src/main/java/edu/icet/repository/ProfilePreviousWorkImageRepository.java
+++ src/main/java/edu/icet/repository/ProfilePreviousWorkImageRepository.java
@@ -1,7 +1,8 @@
 package edu.icet.repository;
 
 import edu.icet.dto.ProfilePreviousWorkImage;
+import edu.icet.entity.ProfilePreviousWorkImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProfilePreviousWorkImageRepository extends JpaRepository<ProfilePreviousWorkImage,Long> {
+public interface ProfilePreviousWorkImageRepository extends JpaRepository<ProfilePreviousWorkImageEntity,Long> {
 }
