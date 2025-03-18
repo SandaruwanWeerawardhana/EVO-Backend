8727041c82cee8c9fe6fa6ed32abf341e19696b3
Danuka <danukakoshitha2004@gmail.com>
2025-03-18 09:41:07 +0530

    add CategoryRepository

---
+++ src/main/java/edu/icet/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package edu.icet.repository;
+
+import edu.icet.entity.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<CategoryEntity,Long> {
+}
