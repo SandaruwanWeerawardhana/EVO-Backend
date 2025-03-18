104e0e4b0097fe046f04f315c9bfb808f980b75f
minindu-alwis <minindualwis@gmail.com>
2025-03-18 09:03:44 +0530

    ProfilePackageRepository

---
+++ src/main/java/edu/icet/repository/ProfilePackageRepository.java
@@ -0,0 +1,8 @@
+package edu.icet.repository;
+
+import edu.icet.dto.ProfilePackages;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfilePackageRepository extends JpaRepository<ProfilePackages,Long> {
+
+}
