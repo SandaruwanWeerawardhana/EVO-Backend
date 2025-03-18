67960921a79d83fcb9820c89001ae6bac4846604
Sash980402 <kesharasasanka120@gmail.com>
2025-03-18 10:05:41 +0530

    create GetTogetherEntity

---
+++ src/main/java/edu/icet/entity/GetTogetherEntity.java
@@ -0,0 +1,27 @@
+package edu.icet.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "get_together")
+public class GetTogetherEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer eventID;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+}
