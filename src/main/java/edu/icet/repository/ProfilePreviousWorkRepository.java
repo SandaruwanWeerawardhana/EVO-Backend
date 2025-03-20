1b1f4fc79108220d26d8624def0a4ec0532ad3b9
Praneeth1221 <praneethravishka@gmail.com>
2025-03-20 11:19:51 +0530

    Create PreviousWorkRepo

--- src/main/java/edu/icet/repository/ProfilePreviousWorkRepository.java
+++ src/main/java/edu/icet/repository/ProfilePreviousWorkRepository.java
@@ -3,6 +3,13 @@ package edu.icet.repository;
 import edu.icet.entity.ProfilePreviousWorkEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ProfilePreviousWorkRepository extends JpaRepository<ProfilePreviousWorkEntity,Long> {
 
+    List<ProfilePreviousWorkEntity> findByProfileID(Long profileID);
+    List<ProfilePreviousWorkEntity> findByCustomerID(Long customerID);
+    List<ProfilePreviousWorkEntity> findByCompletionDateAfter(LocalDate date);
+    List<ProfilePreviousWorkEntity> findByCompletionDateBefore(LocalDate date);
 }
