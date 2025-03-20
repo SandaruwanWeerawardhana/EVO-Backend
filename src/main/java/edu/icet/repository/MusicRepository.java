1cf4f120ef329ffc4c12b3e826c2483275a43c11
Praneeth1221 <praneethravishka@gmail.com>
2025-03-20 10:28:22 +0530

    Create MusicRepository

--- src/main/java/edu/icet/repository/MusicRepository.java
+++ src/main/java/edu/icet/repository/MusicRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+
 public interface MusicRepository extends JpaRepository<MusicEntity,Long> {
     List<MusicEntity> findBySupplierId(Long supplierId);
     List<MusicEntity>findByTeamSize(Integer teamSize);
