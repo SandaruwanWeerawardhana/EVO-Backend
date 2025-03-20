854a434858053057d583b7a4bf4c100ab4008e5c
yetimeh <senukabandara2006@gmail.com>
2025-03-20 13:17:30 +0530

    Integrate beauty salon service with repository Remove BeautySalon getByName

--- src/main/java/edu/icet/repository/BeautySaloonRepository.java
+++ src/main/java/edu/icet/repository/BeautySaloonRepository.java
@@ -3,5 +3,7 @@ package edu.icet.repository;
 import edu.icet.entity.BeautySaloonEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BeautySaloonRepository extends JpaRepository<BeautySaloonEntity,Long> {
 }
