4d58c097e4b58c00c5852b946f3e5d8060807f27
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-03-20 15:29:57 +0530

    QuickReply Service Repository Integrate

--- src/main/java/edu/icet/config/Config.java
+++ src/main/java/edu/icet/config/Config.java
@@ -3,9 +3,7 @@ package edu.icet.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -20,7 +18,7 @@ public class Config {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 		http
-				.csrf(AbstractHttpConfigurer::disable)
+				.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests((auth -> auth.anyRequest().permitAll()));
 //				.oauth2Login(Customizer.withDefaults());
 
