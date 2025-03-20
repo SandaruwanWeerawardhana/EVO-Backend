d950b360961d2ae3f9134c0c4f24184c930a2ad5
Madhubhashini57 <jkpimesha2003@gmail.com>
2025-03-20 10:10:50 -0700

    Update GetTogetherRepository.java

--- src/main/java/edu/icet/repository/GetTogetherRepository.java
+++ src/main/java/edu/icet/repository/GetTogetherRepository.java
@@ -3,10 +3,5 @@ package edu.icet.repository;
 import edu.icet.entity.GetTogetherEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface GetTogetherRepository extends JpaRepository<GetTogetherEntity,Integer> {
-    List<GetTogetherEntity> findAllByTitle(String title);
-    List<GetTogetherEntity> findAllByDescription(String description);
-
 }
