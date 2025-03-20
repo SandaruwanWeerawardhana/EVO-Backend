583b4400bde2cead5404bee7977ab8b1292d0b26
Methminpulsara <methminpulsara10@gmail.com>
2025-03-20 10:35:19 +0530

    Create supplier repositories

---
+++ src/main/java/edu/icet/repository/PropertyImageRepository.java
@@ -0,0 +1,7 @@
+package edu.icet.repository;
+
+import edu.icet.entity.PropertyImageEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropertyImageRepository extends JpaRepository<PropertyImageEntity,Long> {
+}
