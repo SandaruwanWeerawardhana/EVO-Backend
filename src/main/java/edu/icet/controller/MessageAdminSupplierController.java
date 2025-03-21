cab335230ed92f31a30b787b5a5cee24754dd55e
Senuka Bandara <am.senuka.bandara@gmail.com>
2025-03-21 12:37:43 +0530

    Update MessageAdminSupplierController.java

--- src/main/java/edu/icet/controller/MessageAdminSupplierController.java
+++ src/main/java/edu/icet/controller/MessageAdminSupplierController.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 
 
 @RestController
-@RequestMapping("/messageAdminSupplier")
+@RequestMapping("/system/message/admin-supplier")
 @RequiredArgsConstructor
 @CrossOrigin
 public class MessageAdminSupplierController {
