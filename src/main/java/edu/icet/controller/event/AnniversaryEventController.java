f219522cedeb47e5b53c819aa193d2c89a07f4d7
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-03-25 14:14:30 +0530

    update

--- src/main/java/edu/icet/controller/event/AnniversaryEventController.java
+++ src/main/java/edu/icet/controller/event/AnniversaryEventController.java
@@ -1,48 +1,48 @@
-package edu.icet.controller.event;
-
-import edu.icet.dto.event.Anniversary;
-import edu.icet.service.customer.AnniversaryEventService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/anniversary")
-@RequiredArgsConstructor
-@CrossOrigin
-public class AnniversaryEventController {
-
-   private final AnniversaryEventService anniversaryEventService;
-
-    @PostMapping("/save")
-    public boolean addAnniversary(@RequestBody Anniversary anniversary) {
-        return anniversaryEventService.add(anniversary);
-    }
-
-    @GetMapping("/get-all")
-    public List<Anniversary> getAllAnniversaries() {
-        return anniversaryEventService.getAll();
-    }
-
-    @GetMapping("/get/{eventId}")
-    public Anniversary getAnniversary(@PathVariable("eventId") Long eventId) {
-        return anniversaryEventService.get(eventId);
-    }
-
-    @PutMapping("/update")
-    public boolean updateAnniversary(@RequestBody Anniversary anniversary) {
-        return anniversaryEventService.update(anniversary);
-    }
-
-    @DeleteMapping("/delete/{eventId}")
-    public boolean deleteAnniversary(@PathVariable("eventId") Long eventId) {
-       return anniversaryEventService.delete(eventId);
-    }
-}
-
-
-
-
-
-
+//package edu.icet.controller.event;
+//
+//import edu.icet.dto.event.Anniversary;
+//import edu.icet.service.customer.AnniversaryEventService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/anniversary")
+//@RequiredArgsConstructor
+//@CrossOrigin
+//public class AnniversaryEventController {
+//
+//   private final AnniversaryEventService anniversaryEventService;
+//
+//    @PostMapping("/save")
+//    public boolean addAnniversary(@RequestBody Anniversary anniversary) {
+//        return anniversaryEventService.add(anniversary);
+//    }
+//
+//    @GetMapping("/get-all")
+//    public List<Anniversary> getAllAnniversaries() {
+//        return anniversaryEventService.getAll();
+//    }
+//
+//    @GetMapping("/get/{eventId}")
+//    public Anniversary getAnniversary(@PathVariable("eventId") Long eventId) {
+//        return anniversaryEventService.get(eventId);
+//    }
+//
+//    @PutMapping("/update")
+//    public boolean updateAnniversary(@RequestBody Anniversary anniversary) {
+//        return anniversaryEventService.update(anniversary);
+//    }
+//
+//    @DeleteMapping("/delete/{eventId}")
+//    public boolean deleteAnniversary(@PathVariable("eventId") Long eventId) {
+//       return anniversaryEventService.delete(eventId);
+//    }
+//}
+//
+//
+//
+//
+//
+//
--- src/main/java/edu/icet/controller/event/WeddingController.java
+++ src/main/java/edu/icet/controller/event/WeddingController.java
@@ -1,46 +1,46 @@
-package edu.icet.controller.event;
-
-import edu.icet.dto.event.Wedding;
-import edu.icet.service.event.WeddingEventService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-import java.time.LocalDate;
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/wedding")
-@RequiredArgsConstructor
-@CrossOrigin
-public class WeddingController {
-    private final WeddingEventService weddingEventService;
-
-    @GetMapping("/get/{id}")
-    public Wedding get(@PathVariable("id") Long id){
-        return weddingEventService.get(id);
-    }
-
-    @GetMapping("/get-by-date/{date}")
-    public List<Wedding> get(@PathVariable("date") LocalDate date){
-        return weddingEventService.getByDate(date);
-    }
-
-    @GetMapping("/get-all")
-    public List<Wedding> getAll(){
-        return weddingEventService.getAll();
-    }
-
-    @PostMapping("/save")
-    public boolean save(@RequestBody Wedding wedding){
-        return weddingEventService.add(wedding);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public boolean delete(@PathVariable("id") Long id){
-        return weddingEventService.delete(id);
-    }
-
-    @PutMapping("/update")
-    public boolean update(@RequestBody Wedding wedding){
-        return weddingEventService.update(wedding);
-    }
-}
+//package edu.icet.controller.event;
+//
+//import edu.icet.dto.event.Wedding;
+//import edu.icet.service.event.WeddingEventService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.*;
+//import java.time.LocalDate;
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/wedding")
+//@RequiredArgsConstructor
+//@CrossOrigin
+//public class WeddingController {
+//    private final WeddingEventService weddingEventService;
+//
+//    @GetMapping("/get/{id}")
+//    public Wedding get(@PathVariable("id") Long id){
+//        return weddingEventService.get(id);
+//    }
+//
+//    @GetMapping("/get-by-date/{date}")
+//    public List<Wedding> get(@PathVariable("date") LocalDate date){
+//        return weddingEventService.getByDate(date);
+//    }
+//
+//    @GetMapping("/get-all")
+//    public List<Wedding> getAll(){
+//        return weddingEventService.getAll();
+//    }
+//
+//    @PostMapping("/save")
+//    public boolean save(@RequestBody Wedding wedding){
+//        return weddingEventService.add(wedding);
+//    }
+//
+//    @DeleteMapping("/delete/{id}")
+//    public boolean delete(@PathVariable("id") Long id){
+//        return weddingEventService.delete(id);
+//    }
+//
+//    @PutMapping("/update")
+//    public boolean update(@RequestBody Wedding wedding){
+//        return weddingEventService.update(wedding);
+//    }
+//}
