6dfa9c80c45378de486eeed73ae07ad6d0bfc992
NimeshaT <nimeshaijse2020@gmail.com>
2025-03-25 14:19:15 +0530

    Completed GetTogetherEventServiceImpl.java

--- src/main/java/edu/icet/service/event/impl/GetTogetherEventServiceImpl.java
+++ src/main/java/edu/icet/service/event/impl/GetTogetherEventServiceImpl.java
@@ -1,49 +1,58 @@
 package edu.icet.service.event.impl;
 
 import edu.icet.dto.event.GetTogether;
+import edu.icet.entity.event.GetTogetherEntity;
+import edu.icet.repository.event.GetTogetherRepository;
 import edu.icet.service.event.GetTogetherEventService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class GetTogetherEventServiceImpl implements GetTogetherEventService {
-    private final List<GetTogether> getTogetherList = new ArrayList<>();
+
+    final GetTogetherRepository getTogetherRepository;
+    final ModelMapper modelMapper;
 
     @Override
     public boolean add(GetTogether getTogether) {
-        return getTogetherList.add(getTogether);
+
+        GetTogetherEntity entity = getTogetherRepository.save(modelMapper.map(getTogether, GetTogetherEntity.class));
+        getTogetherRepository.save(entity);
+        return true;
+
     }
 
     @Override
     public boolean delete(Integer id) {
-        return getTogetherList.removeIf(getTogether -> getTogether.getEventID().equals(id));
+        if (id == null) {
+            return false;
+        }
+        getTogetherRepository.deleteById(id);
+        return true;
     }
 
     @Override
     public List<GetTogether> getAll() {
-        return getTogetherList;
+        List<GetTogether> togetherList = new ArrayList<>();
+        getTogetherRepository.findAll().forEach(getTogether ->
+                togetherList.add(modelMapper.map(getTogether, GetTogether.class)));
+        return togetherList;
     }
 
     @Override
     public boolean update(GetTogether getTogether) {
-        for (int a=0;a<getTogetherList.size();a++) {
-            if (getTogetherList.get(a).getEventID().equals(getTogether.getEventID())) {
-                getTogetherList.set(a, getTogether);
-                return true;
-            }
-        }
-        return false;
+        getTogetherRepository.save(modelMapper.map(getTogether, GetTogetherEntity.class));
+        return true;
     }
 
     @Override
     public GetTogether get(Integer id) {
-        for (GetTogether getTogether : getTogetherList) {
-                if (getTogether.getEventID().equals(id)) {
-                    return getTogether;
-                }
-            }
-            return null;
+        return getTogetherRepository.findById(id).map(getTogether -> modelMapper.map(getTogether, GetTogether.class)).orElse(null);
         }
     }
 
