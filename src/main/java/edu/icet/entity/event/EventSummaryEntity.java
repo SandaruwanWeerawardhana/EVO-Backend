20f66404e8962958399e136d68570f8a03d537a0
RameshEdirisinghe <e2145434@bit.uom.lk>
2025-03-27 10:52:35 +0530

    commit before merge

--- src/main/java/edu/icet/entity/event/EventSummaryEntity.java
+++ src/main/java/edu/icet/entity/event/EventSummaryEntity.java
@@ -1,13 +1,13 @@
 package edu.icet.entity.event;
 import edu.icet.entity.admin.SupplierRequestEntity;
 import edu.icet.entity.supplier.ProfilePackagesEntity;
-import edu.icet.entity.supplier.SupplierEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+
 
 @Data
 @AllArgsConstructor
@@ -17,21 +17,26 @@ import java.util.List;
 public class EventSummaryEntity {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long eventSummaryId;
 
-//    @Column(nullable = false)
-//    private EventSummaryEntity eventSummaryEntityId;
+    @OneToOne
+    @JoinColumn(name = "event_id",nullable = false)
+    private EventEntity event;
+
+//    @ManyToOne
+//    @JoinColumn(name = "venue_id",nullable = false)
+//    private Long venue;
+
+//    @ManyToOne
+//    @JoinColumn(name = "customer_id",nullable = false)
+//    private Long customerId;
+
+    private Integer headCount;
 //
-//    private EventEntity eventEntityId;
-
-    private Long venueId;
-
-    @Column(nullable = false)
-    private Long customerId;
-
-//    @Column(nullable = false)
-//    @OneToMany
+//    @ManyToMany
 //    private List<ProfilePackagesEntity> packagesList;
+
+//    @OneToMany
 //    private List<SupplierRequestEntity> RequestList;
 
     private Double totalPrice;
