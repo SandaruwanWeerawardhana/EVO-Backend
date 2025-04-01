05ec70d83e965fdde98d5c140943fddc7c77ee41
sandhanuDulmeth <sandhanu12@gmail.com>
2025-04-01 09:36:38 +0530

    commit

--- src/main/java/edu/icet/service/system/MessageCustomerSupplierService.java
+++ src/main/java/edu/icet/service/system/MessageCustomerSupplierService.java
@@ -5,10 +5,7 @@ import java.util.List;
 
 public interface MessageCustomerSupplierService {
     MessageCustomerSupplier sendMessage(MessageCustomerSupplier message);
-    boolean deleteMessage(Long mid );
-    List<MessageCustomerSupplier> getAllMessages();
-    MessageCustomerSupplier getMessageById(Long mid);
-    MessageCustomerSupplier searchMessage(Long mid);
+
     List<MessageCustomerSupplier> getMessagesBySupplierId(Long supplierId);
     List<MessageCustomerSupplier> getMessagesByIds(Long customerId,Long supplierId);
     List<MessageCustomerSupplier> getMessagesByCustomerId(Long customerId);
