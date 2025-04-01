04d964d7247cf2911808604b392d4163e3d5f8be
minindu-alwis <minindualwis@gmail.com>
2025-04-01 09:10:33 +0530

    Fixed admin supplier controller and service

--- src/main/java/edu/icet/service/system/MessageAdminSupplierService.java
+++ src/main/java/edu/icet/service/system/MessageAdminSupplierService.java
@@ -5,10 +5,6 @@ import java.util.List;
 
 public interface MessageAdminSupplierService {
     MessageAdminSupplier sendMessage(MessageAdminSupplier message);
-    boolean deleteMessage(Long mid );
-    List<MessageAdminSupplier> getAllMessages();
-    MessageAdminSupplier getMessageById(Long mid);
-    MessageAdminSupplier searchMessage(Long mid);
     List<MessageAdminSupplier> getMessagesBySupplierId(Long supplierId);
     List<MessageAdminSupplier> getMessagesByIds(Long adminId,Long supplierId);
     List<MessageAdminSupplier> getMessagesByAdminId(Long adminId);
